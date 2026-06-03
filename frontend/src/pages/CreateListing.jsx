9385aad4218adf769cff8b1d38a6192f967243db
Albina93 <alba.tleubergen@gmail.com>
2026-06-03 18:13:37 -0400

    add CreateListing page - working

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,4 +1,132 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../utils/api";
+
 const CreateListing = () => {
-  return <div>Create Listing page</div>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    photo_url: "",
+    quantity: "",
+    pickup_time: "",
+    location: "",
+    accept_donations: false,
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    try {
+      await api.post("/api/listings", formData);
+      navigate("/dashboard");
+    } catch (err) {
+      if (err.response) {
+        setError(err.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <h1>Post food</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Description</label>
+          <input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Photo URL</label>
+          <input
+            type="text"
+            name="photo_url"
+            value={formData.photo_url}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Quantity</label>
+          <input
+            type="text"
+            name="quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Time</label>
+          <input
+            type="text"
+            name="pickup_time"
+            value={formData.pickup_time}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Location</label>
+          <input
+            type="text"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Accepts donations?</label>
+          <input
+            type="checkbox"
+            name="accept_donations"
+            checked={formData.accept_donations}
+            onChange={handleChange}
+          />
+        </div>
+
+        {/* button can't be clicked while submitting  */}
+        <button type="submit" disabled={loading}>
+          {loading ? "Post listing..." : "Post listing"}
+        </button>
+      </form>
+    </div>
+  );
 };
 export default CreateListing;
